5c475deaf8f52075d679761724c3744649ebc494
infinytstudent <169093137+infinytstudent@users.noreply.github.com>
2024-07-24 18:51:20 -0300

    Add files via upload

---
+++ aula 12/biblioteca.js
@@ -0,0 +1,3 @@
+let livros = [{"nome" : "1984","autor" : "George Orwell","ano" : 1949,"genêro" : "ficção científica","avaliação" : ""},
+    {"nome" : "a revolta dos bichos","autor" : "George Orwell","ano" : 1945,"genêro" : "alegoria","avaliação" : ""}
+        ]
